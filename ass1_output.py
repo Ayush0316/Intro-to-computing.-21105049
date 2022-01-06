7291b758f6e3ffab9740daa0d802a789cf61262f
Ayush0316 <95131125+Ayush0316@users.noreply.github.com>
2022-01-06 15:49:29 +0530

    Add files via upload

---
+++ ass1_output.py
@@ -0,0 +1,37 @@
+Python 2.7.18 (v2.7.18:8d21aa21f2, Apr 20 2020, 13:25:05) [MSC v.1500 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============ RESTART: A:\python\school assigment\assignment_1.py ============
+Solution 1.
+Enter first number: 34
+Enter second number: 34
+Enter third number: 54
+('Average of numbers entered by use is: ', 40.666666666666664)
+()
+Solution 2.
+Enter income to the nearest penny: $20000
+Enter number of dependents: 2
+('Your incone tax is $', 800.0)
+()
+Solution 3.
+Enter your SID: 21105049
+Enter your name: "Ayush Kansal"
+Enter your gender: 'M'
+Enter your course name: 'ECE'
+Enter your CGPA: 9.8
+('List containing different datatypes is: ', [21105049, 'Ayush Kansal', 'M', 'ECE', 9.8])
+()
+Solution 4.
+Enter student 1 marks: 78
+Enter student 2 marks: 86
+Enter student 3 marks: 56
+Enter student 4 marks: 88
+Enter student 5 marks: 34
+('Sorted list of students marks: ', [34.0, 56.0, 78.0, 86.0, 88.0])
+()
+Solution 5.a
+('List after removing 4th element: ', ['Red', 'Green', 'White', 'Pink', 'Yellow'])
+()
+Solution 5.b
+('List after replacing Black and Pink with Purple: ', ['Red', 'Green', 'White', 'Purple', 'Purple', 'Yellow'])
+>>> 
