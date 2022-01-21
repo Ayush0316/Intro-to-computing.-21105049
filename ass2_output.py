ccc90e2d31dab8608f6d849bfe6c9fcce2fc6901
Ayush0316 <95131125+Ayush0316@users.noreply.github.com>
2022-01-21 17:53:30 +0530

    Add files via upload

---
+++ ass2_output.py
@@ -0,0 +1,49 @@
+	 Question 1
+Part a
+35 
+
+Part b
+egaugnal evitisnes esac a si nohtyP 
+
+Part c
+a case sensitive 
+
+Part d
+Python is object oriented language 
+
+part e
+10 
+
+part f
+Pythonisacasesensitivelanguage 
+
+	 Question 2
+Hey,Ayush kansal Here! 
+MY SID is 21105049 
+I am form ECE department and my CGPA is 9.9 
+
+	 Question 3
+The valus of a&b is  8
+The valus of a|b is  58
+The valus of a^b is  50
+The value after left shift of a and b by 2 bits is  224  and  40 respectively
+The value after right shift of a by 2 bits and b with 4 bits is  14  and  0 respectively 
+
+	 Question 4
+Enter the first number: 3
+Enter the second number: 5
+Enter the third number: 2
+
+The greatest number is  5 
+
+	 Question 5
+Enter the string: Hello, My name is Ayush Kansal
+
+Yes
+
+	 Question 6
+Enter the length of first side: 34
+Enter the length of second side: 54
+Enter the length of third side: 22
+
+Yes
