4182ceb54bed4b1c4ffb3b0f12455df0d4740d3f
Ayush0316 <95131125+Ayush0316@users.noreply.github.com>
2022-02-02 20:41:54 +0530

    Add files via upload

---
+++ ass3_output.py
@@ -0,0 +1,119 @@
+	 Question 1 
+
+Enter the string:  In technical terms, Python is an object oriented, high level programming language with integrated dynamic semantics primarily for web and app development. It is extremely attractive in the field of Rapid Application Development because it offers dynamic typing and dynamic binding options.
+
+Number of each word/character in string is: {'': 1, 'in': 2, 'technical': 1, 'terms,': 1, 'python': 1, 'is': 2, 'an': 1, 'object': 1, 'oriented,': 1, 'high': 1, 'level': 1, 'programming': 1, 'language': 1, 'with': 1, 'integrated': 1, 'dynamic': 3, 'semantics': 1, 'primarily': 1, 'for': 1, 'web': 1, 'and': 2, 'app': 1, 'development.': 1, 'it': 2, 'extremely': 1, 'attractive': 1, 'the': 1, 'field': 1, 'of': 1, 'rapid': 1, 'application': 1, 'development': 1, 'because': 1, 'offers': 1, 'typing': 1, 'binding': 1, 'options.': 1} 
+
+	 Question 2 
+
+Enter the date: 31
+Enter the month: 12
+Enter the year: 2003
+
+Next day is: 1/1/2004
+
+	 Question 3 
+
+Enter the total number of terms you gonna enter: 5
+
+Enter the 1 number: 3
+Enter the 2 number: 5
+Enter the 3 number: 7
+Enter the 4 number: 9
+Enter the 5 number: 11
+
+[(3, 9), (5, 25), (7, 49), (9, 81), (11, 121)]
+
+	 Question 4 
+
+Enter the grade number: 9
+
+Your grade is 'A' and Excellent Performance.
+
+	 Question 5 
+
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+	 Question 6.
+
+Do you want to enter detials of student?(Y or N): Y
+
+Enter Student's SID: 21105049
+Enter student's name: Ayush kansal
+
+Do you want to enter detials of student?(Y or N): Y
+
+Enter Student's SID: 21105001
+Enter student's name: Ayush kher
+
+Do you want to enter detials of student?(Y or N): y
+
+Please enter valid input.(Y or N)
+
+Do you want to enter detials of student?(Y or N): Y
+
+Enter Student's SID: 21105037
+Enter student's name: Ayush goyal
+
+Do you want to enter detials of student?(Y or N): N
+
+	 Part a 
+
+Student detials are:  {21105049: 'Ayush kansal', 21105001: 'Ayush kher', 21105037: 'Ayush goyal'}
+
+	 Part b 
+
+Sorted dict using students names is:  {21105037: 'Ayush goyal', 21105049: 'Ayush kansal', 21105001: 'Ayush kher'}
+
+	 Part c.
+
+Sorted dict using students SID is:  {21105001: 'Ayush kher', 21105037: 'Ayush goyal', 21105049: 'Ayush kansal'}
+
+	 Part d. 
+
+Enter the Sid whose name you want to search: 21105049
+
+Student's name is: Ayush kansal
+
+	 Question 7 
+
+Enter the the max number of terms you want to print: 8
+
+Fibonacci sequence with 8 terms:
+0
+1
+1
+2
+3
+5
+8
+13
+
+Average of fab series is:  4.125
+
+	 Question 8 
+
+	 Part a 
+
+Set having all the elemets of Set 1 and Set 2 but not both is: {1, 3, 5, 6, 8}
+
+	 Part b 
+
+The set having elements that present only in one sets:  {3, 6, 8, 9, 13, 17}
+
+	 Part c 
+
+The set of elements that are exactly two of the sets Set1, Set2 and Set3:  {1, 2, 4, 5}
+
+	 Part d 
+
+The set having all int less than equal to 10 except that are not in set_1:  {6, 7, 8, 9, 10}
+
+	 Part e 
+
+The set having all int less than equal to 10 except that are not in all the sets:  {10, 7}
