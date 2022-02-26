49d6baf24b9a7fd36a531b0f1a8a02667c4a75d3
Ayush0316 <95131125+Ayush0316@users.noreply.github.com>
2022-02-26 15:08:24 +0530

    Add files via upload

---
+++ ass4_output.py
@@ -0,0 +1,125 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============== RESTART: D:\python\school assigment\assigment_4.py ==============
+	 Question 01 
+
+Move disc 1 from a to c
+Move disc 2 from a to b
+Move disc 1 from c to b
+Move disc 3 from a to c
+Move disc 1 from b to a
+Move disc 2 from b to c
+Move disc 1 from a to c
+
+	 Question 02 
+
+Using recurrsion. 
+
+Enter the number(natural number) of line to print: 5
+
+    1 
+   1 1 
+  1 2 1 
+ 1 3 3 1 
+1 4 6 4 1 
+
+ Using iteration.
+
+    1 
+   1 1 
+  1 2 1 
+ 1 3 3 1 
+1 4 6 4 1 
+
+	 Question 3 
+
+Please enter a number: 97
+Please enter second number(number must not be zero): 4
+
+The quotient and reminder of the values are (24, 1)
+
+	Part A
+
+It is not callable.
+
+	 Part B
+
+Both are non zero.
+
+	 Part C 
+
+Result after adding values: (24, 1, 4, 5, 6) 
+
+List after removing value greater than 4: [1, 4]
+
+
+	 Part D 
+
+After converting the above result into set {1, 4} 
+
+
+	 Part E 
+
+Now set is immutable
+
+	 Part f 
+
+Max value in the set is 4
+Hash value is: 1326775425792931714
+
+	 Question 4 
+
+Object created for Ayush kansal
+Object created for Namit kulshreshtra
+Object created for Harmanpreet singh
+
+The name of the student is Ayush kansal
+The roll number of Ayush kansal is 21105049
+
+
+The name of the student is Namit kulshreshtra
+The roll number of Namit kulshreshtra is 21105025
+
+
+The name of the student is Harmanpreet singh
+The roll number of Harmanpreet singh is 21105053
+
+
+Destructor deleted Ayush kansal's record.
+Destructor deleted Namit kulshreshtra's record.
+Destructor deleted Harmanpreet singh's record.
+
+	 Question 05
+
+Object created named Mehak.
+Object created named Ashok.
+Object created named Viren.
+
+	Showing info of the employee.
+Name of the employee working in this factory is Mehak
+Salary of Mehak is 40000.
+
+	Showing info of the employee.
+Name of the employee working in this factory is Ashok
+Salary of Ashok is 50000.
+
+	Showing info of the employee.
+Name of the employee working in this factory is Viren
+Salary of Viren is 60000.
+
+Updating mehak's salary.
+Updated salary of the employee Mehak is 70000
+
+Calling destructor to delete viren's record.
+Record of the employee Viren is deleted.
+
+	 Question 6 
+
+Frist friend please enter the uttered word: icer
+
+Friend 2 please enter new word to test your friendship: rice
+
+Please enter whether the new word have meaning or not(Y or N): Y
+
+You and your friend passed the friendship test.
